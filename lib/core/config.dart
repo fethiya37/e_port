f526be65176998539f5ede9f508ed5a5d9bcae1f
Emnet-tes <teshomeemnet255@gmail.com>
2026-05-26 11:28:35 +0300

    updated the congig

--- lib/core/config.dart
+++ lib/core/config.dart
@@ -3,30 +3,25 @@ import 'dart:io' show Platform;
 
 class AppConfig {
   static String get baseUrl {
-    // 1) --dart-define override (always wins)
     const fromEnv = String.fromEnvironment('API_BASE_URL');
     if (fromEnv.isNotEmpty) return _stripTrailingSlash(fromEnv);
 
-    // 2) Web
     if (kIsWeb) {
       final host = Uri.base.host;
       final scheme = Uri.base.scheme.isEmpty ? 'http' : Uri.base.scheme;
-      final resolvedHost =
-          (host.isEmpty || host == 'localhost') ? 'localhost' : host;
+      final resolvedHost = (host.isEmpty || host == 'localhost')
+          ? 'localhost'
+          : host;
       return '$scheme://$resolvedHost:3000/api';
     }
 
-    // 3) Android / iOS physical devices
     if (_isAndroid || _isIOS) {
-      // 🟢 Your real PC LAN IP here (with /api)
-      return 'http://10.193.156.7:3000/api';
+      return 'https://eportapi.eportline.com/api';
     }
 
-    // 4) Desktop dev (with /api)
-    if (_isDesktop) return 'http://localhost:3000/api';
+    if (_isDesktop) return 'http://localhost:4000/api';
 
-    // 5) Public fallback (already has /api)
-    return 'https://eportapi.eportapp.com/api';
+    return 'https://eportapi.eportline.com/api';
   }
 
   static bool get _isAndroid {
